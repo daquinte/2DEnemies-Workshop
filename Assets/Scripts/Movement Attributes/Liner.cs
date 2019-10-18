26a5cccac00896b6e3191ba864b36fdafc3c140e
daquinte <daquinte@ucm.es>
2019-10-18 15:47:59 +0200

    componente pacer empezado

--- Assets/Scripts/Movement Attributes/Liner.cs
+++ Assets/Scripts/Movement Attributes/Liner.cs
@@ -17,7 +17,7 @@ public class Liner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/Movement Attributes/Pacer.cs
+++ Assets/Scripts/Movement Attributes/Pacer.cs
@@ -9,14 +9,21 @@ public class Pacer : MonoBehaviour
 {
 
     public float movementSpeed = 0.40f;
+    private GameObject raycastTransform; //TEST
 
     private bool MovingRight = true;
+  
+
     //TODO: PENSAR COMO HACER EL TRANSFORM DELANTE Y LANZAR EL RAYO DESDE AHI
     //https://www.youtube.com/watch?v=aRxuKoJH9Y0
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        raycastTransform = new GameObject("test123");
+        raycastTransform.transform.parent = gameObject.transform; //So that it moves with the gameObject
+        raycastTransform.transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y); //Sigue sin ir :)
+       
     }
 
     // Update is called once per frame
