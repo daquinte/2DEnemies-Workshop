785efda37d52c50432113796fc9f96d95e2a85cb
Daquinte <daquinte@ucm.es>
2019-10-19 21:55:45 +0200

    Componente Pacer hecho. Gizmos en Floater

--- Assets/Scripts/Movement Attributes/Floater.cs
+++ Assets/Scripts/Movement Attributes/Floater.cs
@@ -17,20 +17,30 @@ public class Floater : MonoBehaviour {
 
     public float movementDistance = 4f;              //How long you want the enemy to move upwards from the start position
     public float delayTime = .1f;                    //Time that the entity spends at the edges of the movement
-    public MovementAxis axis = MovementAxis.Y;  // this public var should appear as a drop down
+    public MovementAxis axis = MovementAxis.Y;       // Axis you want to float on
 
-    private string StrAxis;
+    private string StrAxis;                          //String for iTween
+    private Vector3 gizmoFirstPos;  
 
     // Use this for initialization
     void Start () {
 
         Setup();
         iTween.MoveBy(gameObject, iTween.Hash(StrAxis, movementDistance, "easeType", "easeInOutSine", "loopType", "pingPong", "delay", delayTime));
+        
     }
 
     void Setup()
     {
         StrAxis = (axis == MovementAxis.Y) ? "Y" : "X";
+        gizmoFirstPos = transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draws a blue line from this transform to the target
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(gizmoFirstPos, new Vector3(gizmoFirstPos.x, gizmoFirstPos.y + movementDistance));
     }
 
 }
--- Assets/Scripts/Movement Attributes/Pacer.cs
+++ Assets/Scripts/Movement Attributes/Pacer.cs
@@ -3,32 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// The enemy moves in a straight line, but changes direction in response to a trigger.
+/// The enemy moves in a straight line, in the direction you specify, but changes direction in response to a trigger.
 /// </summary>
+/// 
+public enum InitialMovement
+{ 
+    Right, Left
+};
 public class Pacer : MonoBehaviour
 {
 
-    public float movementSpeed = 0.40f;
-    private GameObject raycastTransform; //TEST
+    public float movementSpeed = 0.1f;
+    public InitialMovement initialMovement;
 
-    private bool MovingRight = true;
-  
+    private GameObject raycastEmitter; 
+    //private bool MovingRight;
+    private int groundLayerMask;
 
-    //TODO: PENSAR COMO HACER EL TRANSFORM DELANTE Y LANZAR EL RAYO DESDE AHI
-    //https://www.youtube.com/watch?v=aRxuKoJH9Y0
     // Start is called before the first frame update
     void Start()
     {
+        groundLayerMask = (LayerMask.GetMask("Ground"));
+        Setup();
+    }
 
-        raycastTransform = new GameObject("test123");
-        raycastTransform.transform.parent = gameObject.transform; //So that it moves with the gameObject
-        raycastTransform.transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y); //Sigue sin ir :)
-       
+    void Setup()
+    {
+        //Set the direction
+        float dir = 0f;
+        Debug.Log(transform.eulerAngles);
+        if(transform.eulerAngles.y == 0) { 
+             dir = (initialMovement == InitialMovement.Left) ? -1f : 1f;
+        }
+        else if (transform.eulerAngles.y == 180)
+        {
+             dir = (initialMovement == InitialMovement.Left) ? 1f : -1f;
+
+        }
+        movementSpeed *= dir;
+        Debug.Log(movementSpeed);
+
+
+        //Place the Transform you cast rays from
+        Transform GOTransform = transform;
+        raycastEmitter = new GameObject("raycastEmitter");
+        raycastEmitter.transform.parent = gameObject.transform;
+        switch (initialMovement)
+        {
+            case InitialMovement.Left:
+                
+                raycastEmitter.transform.position = new Vector3(GOTransform.position.x - 1.5f, GOTransform.position.y );
+                break;
+            case InitialMovement.Right:
+                
+                raycastEmitter.transform.position = new Vector3(GOTransform.position.x + 1.5f, GOTransform.position.y - 0.3f);
+
+                break;
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+
+        RaycastHit2D groundRay = Physics2D.Raycast(raycastEmitter.transform.position, Vector2.down, 2, groundLayerMask);
+        if(groundRay.collider == null){ //TODO: exclusivo del suelo
+            transform.Rotate(Vector2.up, 180);
+        }
     }
+
 }
