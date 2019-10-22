c06d3cc33a867858e7de9b88b61f5be3783a32b6
daquinte <daquinte@ucm.es>
2019-10-22 12:48:00 +0200

    Componente Faller usando itween

--- Assets/Scripts/IA/Movement Attributes/Faller.cs
+++ Assets/Scripts/IA/Movement Attributes/Faller.cs
@@ -7,19 +7,23 @@ using UnityEngine;
 /// </summary>
 public class Faller : MonoBehaviour
 {
-    public float fallSpeed;         //downwards speed
-    public int detectionRange;      //Range in which the enemy detects the player
+    public float fallSpeed;                             //downwards speed
+    public int detectionRange;                          //Range in which the enemy detects the player
+
+    private Vector3 originalPosition;                   //The position the entity will return after falling
+    private int playerLayerMask;                        //Player layer for raycast
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        originalPosition = transform.position;
+        playerLayerMask = (LayerMask.GetMask("Player"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D enemyRay = Physics2D.Raycast(transform.position, Vector2.down, detectionRange);
+        RaycastHit2D enemyRay = Physics2D.Raycast(transform.position, Vector2.down, detectionRange, playerLayerMask);
         if(enemyRay.collider != null)
         {
             PlayerManager PM = enemyRay.collider.gameObject.GetComponent<PlayerManager>();
@@ -33,6 +37,18 @@ public class Faller : MonoBehaviour
 
     void FallDown()
     {
-        transform.Translate(Vector2.down * fallSpeed);
+        //transform.Translate(Vector2.down * fallSpeed);
+        //iTween.MoveAdd(gameObject, new Vector3(transform.position.x, transform.position.y - fallSpeed), 1f);
+        iTween.MoveAdd(gameObject, Vector3.down * fallSpeed, 1f);
+        
+        //StartCoroutine("ReturnToOriginalPosition");
+    }
+
+
+   IEnumerator ReturnToOriginalPosition()
+    {
+        yield return new WaitForSecondsRealtime(0.8f);
+        transform.Translate(Vector2.up * fallSpeed);
+        yield return null;
     }
 }
